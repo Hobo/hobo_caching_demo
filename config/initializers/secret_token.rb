a6e31379756606b16752ae62d1068c3a23cd77d6
Bryan Larsen <bryan@larsen.st>
2012-11-26 09:48:27 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoboCachingDemo::Application.config.secret_token = '6d2ab9e22cb933fbfd49b180948f08897edbdd62a1748bece2b63fac1599566287758d252ec504e09d994e61b6d09bcbbd39d148ca5f1b6af0cfc48eb28ee8c4'
